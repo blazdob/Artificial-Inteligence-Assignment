d9443622efe3d6e106400336ebe4b57200e030cd
blazdob <bldobr@gmail.com>
2020-05-24 14:51:43 +0200

    manjši popravek diskretizacije

--- initialize_qvalues.py
+++ initialize_qvalues.py
@@ -9,7 +9,7 @@ count = 0
 # Y -> [0,..200]
 for x in range(0, 400): #vodoravna diskretizacija
     for y in range(0, 200): #vertikalna diskretizacija
-        for vel in range(0, 20): #diskretizacija hitrosti
+        for vel in range(-10, 10): #diskretizacija hitrosti
             # [up, down, right, left, upright, upleft, downright, downleft]
             qval[str(x) + "_" + str(y) + "_" + str(vel)] = [0,0,0,0,0,0,0]
             count += 1
